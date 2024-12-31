b085a85ff3fc99849d7389d98aed32661a3e37f2
Badescu Oliver <olibadescu@gmail.com>
2024-12-31 14:33:13 +0200

    added jwt security, added user roles

--- src/main/java/mycode/stockmanager/app/users/model/User.java
+++ src/main/java/mycode/stockmanager/app/users/model/User.java
@@ -1,8 +1,15 @@
 package mycode.stockmanager.app.users.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.*;
 import lombok.*;
+import mycode.stockmanager.app.system.security.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
 
 import static jakarta.persistence.GenerationType.SEQUENCE;
 
@@ -15,7 +22,7 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 @Builder
 @Table(name = "user")
 @Entity(name = "User")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @SequenceGenerator(
@@ -62,5 +69,53 @@ public class User {
             columnDefinition = "TEXT"
     )
     private String phone;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    public User(Long id, String fullName, String phoneNumber, String email, String password, UserRole userRole, LocalDateTime registeredAt, LocalDateTime createdAt, boolean active) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phoneNumber;
+        this.email = email;
+        this.password = new BCryptPasswordEncoder().encode(password);
+        this.userRole = userRole;
+
+    }
+
+    public void setPassword(String password){
+        this.password= new BCryptPasswordEncoder().encode(password);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return userRole.getGrantedAuthorities();
+    }
+
+    @Override
+    public String getUsername(){
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
